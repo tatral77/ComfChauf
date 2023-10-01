62a8dfb2f519828ef9906cab84b14a3bbd79b050
MASOOD AKHTAR <masoodakhtar77@gmail.com>
2023-10-01 20:38:15 +0500

    Update data.js

--- components/Hoc/Header/data.js
+++ components/Hoc/Header/data.js
@@ -1,15 +1,22 @@
-export const HeaderData = [ { name: "About Us", value:"aboutus", link:"/aboutus" }, { name: "Our Company", value:"ourcompany", link:"/ourcompany" }, { name: "Our Cars", value:"ourcars", link:"/ourcars" }];
-export const OurCarsData = [
-  { name: "Mercedes S-Class", value:"mercedesclass", link:"/mercedesclass" },
-  { name: "Mercedes V-Class & EQV Mercedes", value:"mercedesvclass", link:"/mercedesvclass"  },
-  { name: "E-Class Range Rover", value:"rangerover", link:"/rangerover"  },
-  { name: "Hybrid BMW", value:"hybridbmw", link:"/hybridbmw"  },
-  { name: "i7Bentley", value:"i7bentley", link:"/i7bentley"  },
-  { name: "Mulsanne Rolls-Royce", value:"rollsroyce", link:"/rollsroyce"  },
-  { name: "Phantom", value:"phantom", link:"/phantom"  },
-  
+export const HeaderData = [
+  { name: "About Us", value: "aboutus", link: "/aboutus" },
+  { name: "Our Company", value: "ourcompany", link: "/ourcompany" },
+  { name: "Our Cars", value: "ourcars", link: "/ourcars" },
 ];
-     
+export const OurCarsData = [
+  { name: "Mercedes S-Class", value: "mercedesclass", link: "/mercedesclass" },
+  {
+    name: "Mercedes V-Class & EQV Mercedes",
+    value: "mercedesvclass",
+    link: "/mercedesvclass",
+  },
+  { name: "E-Class Range Rover", value: "rangerover", link: "/rangerover" },
+  { name: "Hybrid BMW", value: "hybridbmw", link: "/hybridbmw" },
+  { name: "i7Bentley", value: "i7bentley", link: "/i7bentley" },
+  { name: "Mulsanne Rolls-Royce", value: "rollsroyce", link: "/rollsroyce" },
+  { name: "Phantom", value: "phantom", link: "/phantom" },
+];
+
 export const AboutUsData = [
   { name: "Login" },
   { name: "Blog" },
@@ -17,6 +24,6 @@ export const AboutUsData = [
   { name: "Terms & conditions" },
 ];
 export const OurCompanyData = [
-  { name: "Contact", value:"contactus", link:"/contactus"  },
+  { name: "Contact", value: "contactus", link: "/contactus" },
   { name: "Terms & conditions" },
 ];
